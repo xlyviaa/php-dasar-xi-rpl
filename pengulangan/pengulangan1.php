789904b7ec375f5ff5d0949c99be6a09733d8e6d
xlyviaa <desviaaprilianti233@gmail.com>
2025-09-15 13:54:06 +0700

    pengulangan
    sintaks php pengulangan

---
+++ pengulangan/pengulangan1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pengulangan 1</title>
+</head>
+<body>
+	<?php
+		for($i = 1; $i<= 10; $i++)
+			echo("<p>Perulangan ke- $i </p>");
+	?>
+</body>
+</html>
---
+++ pengulangan/pengulangan2.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>pengulangan 2</title>
+</head>
+<body>
+	<h1>pengulangan</h1>
+	<h2>for</h2>
+	<?php
+	for ($i= 1; $i <= 5; $i++) { 
+		echo "hallo worid <br>";
+	}
+	?>
+
+	<h2>whlie</h2>
+	<?php
+	$i = 1;
+	while ($i <= 5) {
+		echo "hallo wprld <br>";
+		$i++;
+	}
+	?>
+
+	<h2>do while</h2>
+	<?php
+	$i + 1;
+	do{
+		echo "hallo world <br>";
+		$i++;
+	} while ($i <= 5);
+	?>
+
+</body>
+</html>
