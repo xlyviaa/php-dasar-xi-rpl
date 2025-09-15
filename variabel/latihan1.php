b6e53c6d04f340203ef7460e58e4570cc618db9a
xlyviaa <desviaaprilianti233@gmail.com>
2025-09-15 13:50:03 +0700

    variabel
    sintaks php variabel

---
+++ variabel/latihan1.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>variabel</title>
+</head>
+<body>
+	<?php
+        $nama="xczvia";
+
+        echo $nama;  
+	?>
+</body>
+</html>
---
+++ variabel/latihan2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>variabel latihan 2</title>
+</head>
+<body>
+	<?php
+	    $nama="xczvia";
+	    $kelas="11 rpl";
+	    $alamat="jl.puri surya cigadung blok ab.09,kab.subang kac.subang";
+
+	    echo "nama : $nama";
+	    echo "<br>";
+	    echo "kelas : $kelas";
+	    echo "<br>";
+	    echo "alamat : $alamat";
+	?>
+
+</body>
+</html>
